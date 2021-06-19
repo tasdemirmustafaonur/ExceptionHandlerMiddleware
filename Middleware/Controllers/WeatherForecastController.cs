99aba04dba5f872ac5dc37acf14a6ecc76634a06
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-06-20 02:05:13 +0300

    Add project files.

---
+++ Middleware/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            throw new Exception("Test Hatası");
+
+            return "OK";
+        }
+    }
+}
---
+++ Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate Next)
+        {
+            next = Next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await next.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+
+                //Hata yönetimi
+            }
+        }
+    }
+
+}
